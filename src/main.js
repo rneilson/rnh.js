9364e8d2b210054082903525deda3a478fccd8bf
Raymond Neilson <rneilson@interloper.ca>
2016-09-10 23:25:57 -0400

    Update exports

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 export { h, replace, addchd, remchd, clrchd, brklns, txt, html, t, c, br, a, p, div, span, ul, li, em, strong, img, pre, input, select, textarea } from './html.js';
 export { sel, byid, bycls, bytag } from './select.js';
 export { setprops, addcls, remcls, togcls } from './props.js';
-export { isCallable, hasProp, makeStr, strLike, nodeLike } from './utils.js';
+export { has, makestr, strlike, nodelike } from './utils.js';

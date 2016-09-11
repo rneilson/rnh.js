802fe39364c9368b9819e99ad51d63156eae54c7
Raymond Neilson <rneilson@interloper.ca>
2016-09-10 23:22:46 -0400

    Changed hasProp() to has(); removed isCallable(); lowercased functions

--- src/utils.js
+++ src/utils.js
@@ -1,22 +1,21 @@
 // Utility functions
 
-export function isCallable (fn) {
-	return typeof fn === 'function' || Object.prototype.toString.call(fn) === '[object Function]';
-}
-
-export function hasProp (obj, prop) {
+export function has (obj, prop) {
 	return Object.prototype.hasOwnProperty.call(obj, prop);
 }
 
-export function makeStr (s) {
+export function makestr (s) {
 	return s === undefined ? '' : s === null ? '' : typeof s === 'string' ? s : String(s);
 }
 
-export function strLike (s) {
-	var type = typeof s;
-	return type === 'string' || type === 'number' || type === 'boolean' || s instanceof Date || s instanceof RegExp;
+export function strlike (s) {
+	var t = typeof s;
+	if (t === 'string' || t === 'number' || t === 'boolean') return true;
+	t = Object.prototype.toString.call(s);
+	if (t === '[object Date]' || t === '[object RegExp]') return true;
+	return false;
 }
 
-export function nodeLike (n) {
-	return n && n.nodeName && n.nodeType;
+export function nodelike (n) {
+	return !!(n && n.nodeName && n.nodeType);
 }
